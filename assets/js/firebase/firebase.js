2d1377b92a5e21779b44d98515f2338f8ec5a066
MadeleySV <mashelysalomevasquez@gmail.com>
2024-02-14 20:57:31 -0500

    agregando mas en setup

--- assets/js/firebase/firebase.js
+++ assets/js/firebase/firebase.js
@@ -1,7 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
-import { getFirestore, collection, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import {
+   getFirestore,
+  collection,
+  addDoc,
+  doc,
+  deleteDoc,
+  onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -29,4 +35,6 @@ const firebaseConfig = {
 export const createTask = (title, description) => addDoc(collection(db, "tasks"),{ title, description}); 
 
 export const onGetTask = (callback) => onSnapshot(collection(db, "tasks"),callback);
+
+export const deleteTask = id => deleteDoc(doc(db, "tasks", id));
 
--- assets/js/firebase/setup_tasks.js
+++ assets/js/firebase/setup_tasks.js
@@ -1,4 +1,4 @@
-import { createTask, onGetTask} from "./firebase.js";
+import { createTask, deleteTask, onGetTask} from "./firebase.js";
 
 const taskForm = document.getElementById("create-form");
 const tasksContainer = document.getElementById("tasks-container");
@@ -7,6 +7,7 @@ window.addEventListener("DOMContentLoaded", () => {
     onGetTask((querySnapshot) => {
         let html = '';
 
+        // crear el gred
         querySnapshot.forEach(doc => {
             const data = doc.data();
 
@@ -26,9 +27,17 @@ window.addEventListener("DOMContentLoaded", () => {
         });
 
         tasksContainer.innerHTML = html;
+
+        // DELETE
+        const btnsDelete = document.querySelectorAll(".btn-delete-custom");
+
+        btnsDelete.forEach(btn => {
+            btn.addEventListener("click", ({target: { dataset }}) => deleteTask(dataset.id));
+        });
     });
 });
 
+// create
 taskForm.addEventListener("submit", (e) => {
     // Evitamos que recargue la pagina
     e.preventDefault();
